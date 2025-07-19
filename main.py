8f0cf5fe9f1b439e2baef329d3ce16efd26640c7
Crewlix159 <evdakovai@list.ru>
2025-07-19 16:10:43 +0300

    add_new_functions for check

--- main.py
+++ main.py
@@ -1,27 +1,76 @@
 import telebot
+import random
 
 bot = telebot.TeleBot("8096091257:AAEBeX057wdp3F9BZi24X_V4LgWGihiDiDo")
 
-@bot.message_handler(commands=["start"])
-def send_welcome(message):
-    msg = """
-Привет!
-Я твой Бот!
-Вам доступны следующие комманды:
-    /start
-    /hello
-    /bye"""
-    bot.reply_to(message, msg)
-@bot.message_handler(commands=["hello"])
-def send_hello(message):
-    msg = "Тебе тоже привет!"
-    bot.reply_to(message, msg)
-@bot.message_handler(commands=['bye'])
-def send_bye(message):
-    msg = "И тебе удачи, Броо!"
-    bot.reply_to(message, msg)
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
-#hi
+import os
+
+
+text_messages = {
+    'welcome':
+        u'Please welcome {name}!\n\n'
+        u'This chat is intended for questions about and discussion of the pyTelegramBotAPI.\n'
+        u'To enable group members to answer your questions fast and accurately, please make sure to study the '
+        u'project\'s documentation (https://github.com/eternnoir/pyTelegramBotAPI/blob/master/README.md) and the '
+        u'examples (https://github.com/eternnoir/pyTelegramBotAPI/tree/master/examples) first.\n\n'
+        u'I hope you enjoy your stay here!',
+
+    'info':
+        u'My name is TeleBot,\n'
+        u'I am a bot that assists these wonderful bot-creating people of this bot library group chat.\n'
+        u'Also, I am still under development. Please improve my functionality by making a pull request! '
+        u'Suggestions are also welcome, just drop them in this group chat!',
+
+    'wrong_chat':
+        u'Hi there!\nThanks for trying me out. However, this bot can only be used in the pyTelegramAPI group chat.\n'
+        u'Join us!\n\n'
+        u'https://telegram.me/joinchat/067e22c60035523fda8f6025ee87e30b'
+}
+
+def is_api_group(chat_id):
+    print(chat_id)
+    return chat_id == 1
+
+@bot.message_handler(func=lambda m: True, commands=['start'])
+def on_user_joins(message):
+    if not is_api_group(message.chat.id):
+        return True
+
+    name = message.new_chat_participant.first_name
+    if hasattr(message.new_chat_participant, 'last_name') and message.new_chat_participant.last_name is not None:
+        name += u" {}".format(message.new_chat_participant.last_name)
+
+    if hasattr(message.new_chat_participant, 'username') and message.new_chat_participant.username is not None:
+        name += u" (@{})".format(message.new_chat_participant.username)
+
+    bot.reply_to(message, text_messages['welcome'].format(name=name))
+
+
+@bot.message_handler(commands=['info', 'help'])
+def on_info(message):
+    if not is_api_group(message.chat.id):
+        bot.reply_to(message, text_messages['wrong_chat'])
+        return
+
+    bot.reply_to(message, text_messages['info'])
+
+
+@bot.message_handler(commands=["ping"])
+def on_ping(message):
+    bot.reply_to(message, "Still alive and kicking!")
+
+
+@bot.message_handler(commands=['start'])
+def on_start(message):
+    if not is_api_group(message.chat.id):
+        bot.reply_to(message, text_messages['wrong_chat'])
+        return
+
+
+def listener(messages):
+    for m in messages:
+        print(str(m))
+
+
+bot.set_update_listener(listener)
 bot.polling()
